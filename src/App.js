d3bd614c917662de3755a172013196014321b022
Savindu Gunawardena <savibashnon11@gmail.com>
2021-03-06 20:36:09 +0530

    Add bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Header } from './components/layout/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { About } from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className=""></header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <div className="container">
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
